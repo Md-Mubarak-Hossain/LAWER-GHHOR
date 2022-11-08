49401f8f853a74e40590d790e977645c6c8d6cc6
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-11-08 19:36:13 +0600

    civil law section added

---
+++ src/pages/Services/CivilLaw/CivilLaw.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CivilLaw = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CivilLaw;
---
+++ src/pages/Services/LegalNotice/LegalNotice.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LegalNotice = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default LegalNotice;
