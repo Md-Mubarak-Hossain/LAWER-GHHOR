5d6a732a4eb0d6af0162e8bc7f0cb979b1bb9d41
MD MUBARAK HOSSAIN <hossainmubarak477@gmail.com>
2024-03-25 17:57:23 +0600

    added

--- src/pages/Services/LegalNotice/LegalNotice.js
+++ src/pages/Services/LegalNotice/LegalNotice.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import useTitle from '../../../hooks/useTitle';
-
+import notice from "../../../images/notice.png"
+import { Link } from 'react-router-dom';
 const LegalNotice = () => {
     useTitle('Legal Notice');
     return (
-        <div className='my-10'>
-            <div className="card w-11/12  shadow-xl mx-auto grid grid-cols-1 lg:grid-cols-2">
-                <div className="card-body border p-2">
-                    <h2 className="card-title">Why is it important to obtain a Succession Certificate?</h2>
-                    <p>Succession certificate entitles the holder to make payment of debt or transfer securities to the holder of certificate without having to ascertain the legal heir entitled to it. It provides indemnity to all persons owing such debts or liable on such securities with regards to all payments made to or dealings had in good faith with a person to whom a certificate as granted. Hence, many organisations and person request for succession certificate before settling the debts or securities of the deceased in favour of the person claiming such debts or securities.</p>
+        <div className='p-5'>
+            <div className="w-11/12  bg-base-100-xl mx-auto">
+            <div className='max-h-20'>
+                <img src={notice} alt='notice' className='bg-blue-500 max-h-20' />
+            </div>
+                <div className="py-5">
+                    <h2 className=" text-blue-800">Why is it important to obtain a Succession Certificate?</h2>
+                    <p>Succession certificate entitles the holder to make payment of debt or transfer securities to the holder of certificate without having to ascertain the legal heir entitled to it. It provides indemnity to all persons owing such debts or liable on such securities with regards to all payments made to or dealings had in good faith with a person to whom a certificate as granted. Hence, many organizations and person request for succession certificate before settling the debts or securities of the deceased in favour of the person claiming such debts or securities.</p>
                 </div>
-                <div className="card-body border p-2">
-                    <h2 className="card-title">What is the procedure for obtaining the succession certificate?</h2>
+                <div className="py-5">
+                    <h2 className="text-blue-800">What is the procedure for obtaining the succession certificate?</h2>
                     <p>To obtain succession certificate, a petition has to be filed before the District Judge within whose jurisdiction the deceased person ordinarily resided at the time of his or her death or, if at that time he or she had no fixed place of residence, the District Judge within whose jurisdiction any part of the property of the deceased may be found.</p>
+                </div>
+                <div>
+                    <Link>See more</Link>
                 </div>
             </div>
         </div>
